eb20bd4557572f089a9cf91b8bdd8f8469218bc0
Pedro Valério <pedroddjkK@gmail.com>
2023-07-16 17:02:50 -0300

    Auth.js implemented

---
+++ src/hooks.server.ts
@@ -0,0 +1,25 @@
+import { SvelteKitAuth } from '@auth/sveltekit';
+import Credentials from '@auth/core/providers/credentials';
+
+export const handle = SvelteKitAuth({
+	providers: [
+		Credentials({
+			id: 'credentials',
+			name: 'Credentials',
+			credentials: {
+				username: { label: 'Username', type: 'text' },
+				password: { label: 'Password', type: 'password' }
+			},
+			async authorize(credentials) {
+				if (credentials.username === 'admin' && credentials.password === 'admin') {
+					return {
+						id: 'admin',
+            username: 'admin',
+            roles: ['admin']
+					};
+				}
+				return null;
+			}
+		})
+	]
+});
